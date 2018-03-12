ab2006ff94c2466edd915884bc9999a014ba78a1
Chris H <chuang4150@gmail.com>
2018-03-12 03:26:28 -0500

    Added some JS, non-functional. Also added in tab effects.

---
+++ index.js
@@ -0,0 +1,7 @@
+function returnHomePage() {
+    document.querySelector('#Navigator').pushPage('homepage.html');
+}
+
+function returnAboutPage(){
+    document.querySelector('#Navigator').pushPage('about.html');
+}
