3ae7efd8461b201f5ada0d90c16abf873320a537
Chris H <chuang4150@gmail.com>
2018-04-16 05:24:58 -0500

    Added some Firebase Database functionality, saves the name, email and password but not in the list I want it to so I have to fix that eventually.

--- assets/scripts/form.js
+++ assets/scripts/form.js
@@ -1,9 +1,37 @@
-    // Get the modal
-    var modal = document.getElementById('id01');
-        
-    // When the user clicks anywhere outside of the modal, close it
-    window.onclick = function(event) {
-        if (event.target == modal) {
-            modal.style.display = "none";
-        }
+// Get the modal
+var modal = document.getElementById('id01');
+    
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
     }
+}
+
+var email = document.getElementById("email");
+
+var password = document.getElementById("password");
+
+function submitClick() {
+
+    var firebaseRef = firebase.database().ref();
+
+    var authSave = email.value;
+
+    var passSave = password.value;
+
+    var firstName = first.value;
+
+    var lastName = last.value;
+
+    firebaseRef.push().set(authSave);
+
+    firebaseRef.push().set(passSave);
+
+    firebaseRef.push().set(lastName);
+
+    firebaseRef.push().set(firstName);
+
+    window.alert("Working");
+
+}
