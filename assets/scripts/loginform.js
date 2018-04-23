bc9dede14e4916963224ebafdef9b95aed30a943
dderda <dderda@luc.edu>
2018-04-23 03:10:44 -0500

    Add files via upload
    Cool effects for Login.html

---
+++ assets/scripts/loginform.js
@@ -0,0 +1,26 @@
+//just some rad effects
+function field_focus(field, email)
+  {
+    if(field.value == email)
+    {
+      field.value = '';
+    }
+  }
+
+  function field_blur(field, email)
+  {
+    if(field.value == '')
+    {
+      field.value = email;
+    }
+  }
+
+//Fade in Login box
+$(document).ready(function(){
+    $('.box').hide().fadeIn(1000);
+    });
+
+//Stop click event
+$('a').click(function(event){
+    event.preventDefault(); 
+	});
